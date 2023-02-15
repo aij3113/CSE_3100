1c335937c31dabceb13f7ac1b885f7d3d90338f4
Md. Ariful Islam Joy <60298745+aij3113@users.noreply.github.com>
2023-02-15 19:05:42 +0600

    Update info.py

--- Course Registration/C_Reg/info.py
+++ Course Registration/C_Reg/info.py
@@ -1,7 +1,7 @@
 
 EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS       = True
-EMAIL_HOST          = 'smtp.gmail.com'
-EMAIL_HOST_USER     = 'ruet.c.reg@gmail.com'
-EMAIL_HOST_PASSWORD = 'ehinjdawticuufgm'
+EMAIL_HOST          = ''
+EMAIL_HOST_USER     = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT          = 587
